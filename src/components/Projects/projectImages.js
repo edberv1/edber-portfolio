b4ffa6ac3ca79e99e5957325d55bb3850d763d7d
Edber Vuciterna <vuciternaedber@gmail.com>
2024-06-15 18:00:38 +0200

    added some projects

--- src/components/Projects/projectImages.js
+++ src/components/Projects/projectImages.js
@@ -2,10 +2,18 @@ import one from "../../assets/projects/1.png";
 import two from "../../assets/projects/2.png";
 import three from "../../assets/projects/3.png";
 import four from "../../assets/projects/4.png";
+import five from "../../assets/projects/5.png";
+import six from "../../assets/projects/6.png";
+import seven from "../../assets/projects/7.png";
+import eight from "../../assets/projects/8.png";
 
 export {
   one,
   two,
   three,
   four,
+  five,
+  six,
+  seven,
+  eight
 };
--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -2,7 +2,11 @@ import {
   one,
   two,
   three,
-  four
+  four,
+  five, 
+  six,
+  seven,
+  eight
 } from "./projectImages";
 
 export const projectData = [
@@ -14,7 +18,7 @@ export const projectData = [
       "Heilmassage-FW is a personal website that I made for a client that provides massage services.",
     codeLink: "https://github.com/edberv1/Heilmassage-FW",
     liveLink: "https://heilmassage-fw.vercel.app/",
-    techStack: ["HTML", "CSS", "JavaScript", "ReactJs"],
+    techStack: ["ReactJs", "TailwindCSS"],
   },
   {
     id: 2,
@@ -23,7 +27,24 @@ export const projectData = [
     title:
       "BuySell website is an online platform where individuals and businesses can buy, sell or trade vehicles. The website aims to create a user-friendly experience for customers who want to purchase a vehicle or sell their existing one.",
     codeLink: "https://github.com/edberv1/BuySell",
-    liveLink: "",
     techStack: ["Laravel", "TailwindCSS", "AlpineJS"],
+  },
+  {
+    id: 3,
+    image: five,
+    transitionImage: six,
+    title:
+    "DevLobby is a vibrant online hub designed for programmers to engage, challenge, and enrich their coding skills. It offers a dynamic environment where members can tackle theoretical puzzles and practical coding problems, while also earning rewards such as achievements. DevLobby aims to provide an entertaining and competitive platform that fosters learning and collaboration among developers.",
+    codeLink: "https://github.com/edberv1/DevLobby",
+    techStack: ["MongoDB", "Express", "React.js", "Node.js"],
+  },
+  {
+    id: 4,
+    image: eight,
+    transitionImage: seven,
+    title:
+    "Pharmacy is an innovative app that revolutionizes the way users access healthcare products. It allows customers to conveniently log in, browse, and purchase medicines online with the ease of a credit card transaction. Additionally, PharmaDiv empowers users to step into the role of pharmacists, offering a platform to sell their own health products, creating a comprehensive marketplace for all healthcare needs.",
+    codeLink: "https://github.com/edberv1/Pharmacy",
+    techStack: ["MongoDB", "Express", "React.js", "Node.js", "MySQL"],
   },
 ];
